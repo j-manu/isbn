27e8972a06f9cc99f136a60c073293d5dd285300
Manu J <manu.1982@gmail.com>
2012-10-07 09:27:55 +0530

    remove rediff and indiaplaza, correct landmark search pattern

--- app/store.rb
+++ app/store.rb
@@ -1,6 +1,8 @@
 require 'multi_json'
 class Store
-  INFO_STORES = { flipkart: { url: 'http://www.flipkart.com/search.php?query=[isbn]', book_name: 'div.mprod-summary-title h1', author_name: 'div.mprod-summary-title .primary-info a', image: '#visible-image-small' } }
+  INFO_STORES = { flipkart: { url: 'http://www.flipkart.com/search.php?query=[isbn]',
+                              book_name: 'div.mprod-summary-title h1', author_name: 'div.mprod-summary-title .primary-info a',
+                              image: '#visible-image-small' } }
 
   STORES = {flipkart: {
               url: 'http://www.flipkart.com/search.php?query=[isbn]',
@@ -8,21 +10,12 @@ class Store
             infibeam: {
               url: 'http://www.infibeam.com/Books/search?q=[isbn]',
               pattern: '#infiPrice'},
-            indiaplaza: {
-              url: 'http://www.indiaplaza.com/searchproducts.aspx?sn=books&q=[isbn]',
-              pattern: 'div.ourPrice'},
             crossword: {
               url: 'http://www.crossword.in/books/search?q=[isbn]',
               pattern: '.variant-final-price'},
-            rediff: {
-              url: 'http://books.rediff.com/book/ISBN:[isbn]',
-              pattern: 'font#book-pric b'},
-            indiaplaza: {
-              url: 'http://www.indiaplaza.com/searchproducts.aspx?sn=books&q=[isbn]',
-              pattern: 'div.ourPrice'},
             landmark: {
               url: 'http://www.landmarkonthenet.com/product/SearchPaging.aspx?code=[isbn]&type=0&num=0',
-              pattern: '.price .current-price'},
+              pattern: '.pricebox .price-current .WebRupee-print'},
             homeshop18: {
               url: 'http://www.homeshop18.com/search:[isbn]/',
               pattern: '.pdp_details_price .pdp_details_hs18Price'}
